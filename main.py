ac5e19f0d2535144e0af2317e63b9628877bd11e
Arnav Nagpurkar <arnav.nagpurkar@gmail.com>
2023-10-07 15:13:40 +0530

    Condition 3 completed

--- main.py
+++ main.py
@@ -100,6 +100,7 @@ def condition1():
                 print("Computer won the match!")
                 break
             if computer_wickets == 2:
+                print("Congrats, You won the match!")
                 break
 
             if overs == 0.6:
@@ -135,7 +136,80 @@ def condition2():
 
 # Condition 3 - computer won the toss and choose to bat first
 def condition3():
-    pass
+    global overs, user_score, user_wickets, computer_score, computer_wickets, innings
+    print("\nBowling time:")
+    print("You get 2 overs (12 balls) and computer will set a target for you")
+    print("If tyou chase the target, you win. If not, then computer win.")
+    print("If both your and computer's score is the same, it's a tie.\n")
+    
+    overs = 0.0  # Reset overs for bowling
+
+    for over in range(2):  # 2 overs
+        for ball in range(6):  # 6 balls in an over
+            if computer_wickets == 2:
+                break
+
+            if overs == 0.6:
+                overs = 1.1
+            else:
+                overs += 0.1  # Increment the overs by 0.1 at the start of each ball
+            print(f"Here comes {round(overs, 1)}")  # Display overs in the desired format
+            user_inp = int(input("Enter a number between 1-6 to take wicket: "))
+            comp_inp = random.randint(1, 6)
+            
+            if user_inp < 1 or user_inp > 6:
+                print("Invalid Option selected")
+                continue
+            
+            if user_inp == comp_inp:
+                print("Computer are out")
+                computer_wickets += 1
+                display_scoreboard(user_score, user_wickets, overs, computer_score, computer_wickets, innings)
+            else:
+                computer_score += comp_inp  # Increment computer's score
+                display_scoreboard(user_score, user_wickets, overs, computer_score, computer_wickets, innings)
+
+    print(f"Your bowling over is finished, computer's final score is\n{computer_score}/{computer_wickets}")
+    print(f"You have a target of {user_score + 1} runs!")
+
+    overs = 0.0  # Reset overs for batting
+
+    print("\nBatting time:")
+    for over in range(2):  # 2 overs
+        for ball in range(6):  # 6 balls in an over
+            if user_score > computer_score:
+                print("Congrats, You won the match!")
+                break
+            if user_wickets == 2:
+                print("Computer won the match")
+                break
+
+            if overs == 0.6:
+                overs = 1.1
+            else:
+                overs += 0.1  # Increment the overs by 0.1 at the start of each ball
+            print(f"Here comes {round(overs, 1)}")  # Display overs in the desired format
+            user_inp = int(input("Enter a number between 1-6 to score: "))
+            comp_inp = random.randint(1, 6)
+            
+            if user_inp < 1 or user_inp > 6:
+                print("Invalid Option selected")
+                continue
+            
+            if comp_inp == user_inp:
+                print("You are out")
+                user_wickets += 1
+                display_scoreboard(user_score, user_wickets, overs, computer_score, computer_wickets, innings)
+            else:
+                user_score += user_inp  # Increment computer's score
+                display_scoreboard(user_score, user_wickets, overs, computer_score, computer_wickets, innings)
+
+    if user_score == computer_score:
+        print("The match is a tie!")
+    elif computer_score > user_score:
+        print("Computer won the match!")
+    else:
+        print("Congrats, You won the match!!")
 
 # Condition 4 - computer won the toss and choose to bowl first
 def condition4():
@@ -152,15 +226,15 @@ def main():
         condition1()
 
     # Condition 2 - user won the toss and choose to bowl first
-    if toss == 'user' and user_decision == 'bowl':
+    elif toss == 'user' and user_decision == 'bowl':
         condition2()
 
     # Condition 3 - computer won the toss and choose to bat first
-    if toss == 'computer' and computer_decision == 'bat':
+    elif toss == 'computer' and computer_decision == 'bat':
         condition3()
 
     # Condition 4 - computer won the toss and choose to bowl first
-    if toss == 'computer' and computer_decision == 'bowl':
+    elif toss == 'computer' and computer_decision == 'bowl':
         condition4()
 
 if __name__ == '__main__':
